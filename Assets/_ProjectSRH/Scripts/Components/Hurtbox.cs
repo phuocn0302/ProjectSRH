8aeedf89bc10d32c44bb0092e84b6e465506602e
phuocn0302 <phuocnguyen230302@gmail.com>
2025-01-25 12:48:06 +0700

    fix: knockback not apply correctly

--- Assets/_ProjectSRH/Scripts/Components/Hurtbox.cs
+++ Assets/_ProjectSRH/Scripts/Components/Hurtbox.cs
@@ -27,10 +27,10 @@ public class Hurtbox : MonoBehaviour
         if (!canBeKnockback) return;
         if (transform.parent.TryGetComponent<Rigidbody2D>(out var body)) 
         {
-            Vector3 knockback = (transform.position - knockbackForce.position).normalized;
+            Vector2 knockback = (transform.position - knockbackForce.position).normalized;
 
             knockback = knockback.normalized * knockbackAmount;
-            Tween.Position(body.transform, body.transform.position + knockback, 0.5f);
+            Tween.Position(body.transform, (Vector2)body.transform.position + knockback, 0.5f);
         }
     }
 
